7eaf9c23eb368d3dac649e5808d562978300465d
Bogdan Finn <bogdan.finn@googlemail.com>
2022-09-04 13:49:12 +0200

    define custom client instead of only ja3string;

--- cffi/example_node/index_custom_client.js
+++ cffi/example_node/index_custom_client.js
@@ -59,21 +59,36 @@ const tlsClientLibrary = ffi.Library('./../dist/tls-client-darwin-amd64.dylib', 
   "requestBody": "", // needs to be a string! so json.dumps(yourActualyRequestBody) here
   "requestMethod": "GET"
 }
-
-The Response from the library looks like that:
-{
-  "sessionId": "some reusable sessionId",
-  "status": 200, //In case of an error the status code will be 0
-  "body": "The Response as string here or the error message",
-  "headers": {},
-  "cookies": {}
-}
 */
 const requestPayload = {
-    "tlsClientIdentifier": "chrome_103",
+    "customTlsClient": {
+        "ja3String": "771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,0-23-65281-10-11-35-16-5-13-18-51-45-43-27-17513,29-23-24,0",
+        "h2Settings": {
+            1: 65536,
+            3: 1000,
+            4: 6291456,
+            6: 262144
+        },
+        "h2SettingsOrder": [
+            1,
+            3,
+            4,
+            6
+        ],
+        "pseudoHeaderOrder": [
+            ":method",
+            ":authority",
+            ":scheme",
+            ":path"
+        ],
+        "connectionFlow": 15663105,
+        "priorityFrames": [
+
+        ]
+    },
+    "proxyUrl": "",
     "followRedirects": false,
     "timeoutSeconds": 30,
-    "proxyUrl": "",
     "headers": {},
     "headerOrder": [],
     "requestUrl": "https://tls.peet.ws/api/all",
--- cffi/example_python/example_custom_client.py
+++ cffi/example_python/example_custom_client.py
@@ -13,10 +13,34 @@ request.restype = ctypes.c_char_p
 """ full payload example
 {
   "sessionId": "reusableSessionId",
-  "tlsClientIdentifier": "chrome_103",
+  "tlsClientIdentifier": "chrome_105",
   "followRedirects": False,
   "timeoutSeconds": 30,
-  "ja3String": "771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,0-23-65281-10-11-35-16-5-13-18-51-45-43-27-17513,29-23-24,0",
+  "customTlsClient": {
+    "ja3String": "771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,0-23-65281-10-11-35-16-5-13-18-51-45-43-27-17513,29-23-24,0",
+    "h2Settings": {
+      1: 65536,
+      3: 1000,
+      4: 6291456,
+      6: 262144
+    },
+    "h2SettingsOrder": [
+      1,
+      3,
+      4,
+      6
+    ],
+    "pseudoHeaderOrder": [
+      ":method",
+      ":authority",
+      ":scheme",
+      ":path"
+    ],
+    "connectionFlow": 15663105,
+    "priorityFrames": [
+      
+    ]
+  },
   "proxyUrl": "",
   "headerOrder": [
     "key1",
@@ -36,12 +60,36 @@ request.restype = ctypes.c_char_p
     }
   ],
   "requestUrl": "https://tls.peet.ws/api/all",
-  "requestBody": "",
+  "requestBody": "", // needs to be a string! so json.dumps(yourActualyRequestBody) here
   "requestMethod": "GET"
 }
 """
 requestPayload = {
-    "ja3String": "771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,0-23-65281-10-11-35-16-5-13-18-51-45-43-27-17513,29-23-24,0",
+    "customTlsClient": {
+        "ja3String": "771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,0-23-65281-10-11-35-16-5-13-18-51-45-43-27-17513,29-23-24,0",
+        "h2Settings": {
+            1: 65536,
+            3: 1000,
+            4: 6291456,
+            6: 262144
+        },
+        "h2SettingsOrder": [
+            1,
+            3,
+            4,
+            6
+        ],
+        "pseudoHeaderOrder": [
+            ":method",
+            ":authority",
+            ":scheme",
+            ":path"
+        ],
+        "connectionFlow": 15663105,
+        "priorityFrames": [
+
+        ]
+    },
     "proxyUrl": "",
     "followRedirects": False,
     "timeoutSeconds": 30,
