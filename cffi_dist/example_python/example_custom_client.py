5d3383c38e39911cf501e5280fd0a85cbadc0685
Bogdan Finn <bogdan.finn@googlemail.com>
2022-10-01 20:38:13 +0200

    implemented more granular custom client definition;

--- cffi_dist/example_python/example_custom_client.py
+++ cffi_dist/example_python/example_custom_client.py
@@ -14,10 +14,46 @@ getCookiesFromSession.argtypes = [ctypes.c_char_p]
 getCookiesFromSession.restype = ctypes.c_char_p
 
 requestPayload = {
-    "tlsClientIdentifier": "chrome_105",
     "followRedirects": False,
     "insecureSkipVerify": False,
     "timeoutSeconds": 30,
+    "sessionId": "2my-session-id",
+    "customTlsClient": {
+        "ja3String": "771,2570-4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,2570-0-23-65281-10-11-35-16-5-13-18-51-45-43-27-17513-2570-21,2570-29-23-24,0",
+        "h2Settings": {
+            "HEADER_TABLE_SIZE": 65536,
+            "MAX_CONCURRENT_STREAMS": 1000,
+            "INITIAL_WINDOW_SIZE": 6291456,
+            "MAX_HEADER_LIST_SIZE": 262144
+        },
+        "h2SettingsOrder": [
+            "HEADER_TABLE_SIZE",
+            "MAX_CONCURRENT_STREAMS",
+            "INITIAL_WINDOW_SIZE",
+            "MAX_HEADER_LIST_SIZE"
+        ],
+        "supportedSignatureAlgorithms": [
+            "ECDSAWithP256AndSHA256",
+            "PSSWithSHA256",
+            "PKCS1WithSHA256",
+            "ECDSAWithP384AndSHA384",
+            "PSSWithSHA384",
+            "PKCS1WithSHA384",
+            "PSSWithSHA512",
+            "PKCS1WithSHA512",
+        ],
+        "supportedVersions": ["GREASE", "1.3", "1.2"],
+        "keyShareCurves": ["GREASE", "X25519"],
+        "certCompressionAlgo": "brotli",
+        "pseudoHeaderOrder": [
+            ":method",
+            ":authority",
+            ":scheme",
+            ":path"
+        ],
+        "connectionFlow": 15663105,
+        "priorityFrames": []
+    },
     "proxyUrl": "",
     "headers": {
         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
@@ -31,7 +67,7 @@ requestPayload = {
         "accept-encoding",
         "accept-language"
     ],
-    "requestUrl": "https://tls.peet.ws/api/all",
+    "requestUrl": "https://microsoft.com",
     "requestMethod": "GET",
     "requestBody": "",
     "requestCookies": []
